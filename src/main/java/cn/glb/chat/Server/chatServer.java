6fea5b9c4847c93362e0bb259a0d8cc11f3ae616
bb <2443014708@qq.com>
2020-03-07 12:11:35 +0800

    2020/03/07 chat01

---
+++ src/main/java/cn/glb/chat/Server/chatServer.java
@@ -0,0 +1,12 @@
+package cn.glb.chat.Server;
+
+/**
+ * @author GeLinBo
+ * @date 2020/3/7 12:08
+ */
+public class chatServer {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
